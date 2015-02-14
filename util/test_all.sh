417778830792992fea3e03ee912811cb5cc43aeb
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2015-02-14 12:45:47 +0100

    util dir

--- util/test_all.sh
+++ util/test_all.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #examples (run from base git respository directory!)
-# ./testing/test_all.sh
-# ./testing/test_all.sh qemu
-# ./testing/test_all.sh qemu debug=yes
-# ./testing/test_all.sh qemu debug=yes PROG_START_FUNC=run_all
+# ./util/test_all.sh
+# ./util/test_all.sh qemu
+# ./util/test_all.sh qemu debug=yes
+# ./util/test_all.sh qemu debug=yes PROG_START_FUNC=run_all
+
+ARCH=i386
 
 for chapter in * ; do
   if [ -d $chapter ] ; then
-    #echo $chapter
     cd $chapter
 
     for increment in * ; do
@@ -21,20 +22,22 @@ for chapter in * ; do
         echo "Test $chapter/$increment: starting"
         echo
 
-        if [ -e build.sh ] ; then
-          ./build.sh cleanall
-          ./build.sh $1
-          if [ ! $? -eq 0 ] ; then
-            exit
-          fi
-          ./build.sh cleanall
-        elif [ -e Makefile ] ; then
+        if [ -e Makefile ] ; then
           make cleanall
           make "$@"
           if [ ! $? -eq 0 ] ; then
             exit
           fi
           make cleanall
+        elif [ -e $ARCH/build.sh ] ; then
+          cd $ARCH
+          ./build.sh cleanall
+          ./build.sh $1
+          if [ ! $? -eq 0 ] ; then
+            exit
+          fi
+          ./build.sh cleanall
+          cd ..
         fi
 
         echo
